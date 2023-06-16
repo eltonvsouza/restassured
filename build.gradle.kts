ba6034b95f4336955ced9ac88d213a1d8f4c0e37
Elton Souza <esouza@qac.com>
2023-06-16 16:20:36 -0400

    Remove commented code Add properties to gitignore

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,21 +5,18 @@ plugins {
 group = "org.example"
 version = "1.0-SNAPSHOT"
 
-val cucumberRuntime by configurations.creating {
-    extendsFrom(configurations["testImplementation"])
-}
+//val cucumberRuntime by configurations.creating {
+//    extendsFrom(configurations["testImplementation"])
+//}
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-//    testImplementation(platform("org.junit:junit-bom:5.9.1"))
-//    testImplementation("org.junit.jupiter:junit-jupiter")
     implementation("org.springframework.boot:spring-boot-autoconfigure:3.1.0")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test:3.1.0")
-//    testImplementation("org.junit.platform:junit-platform-suite:1.9.3")
     testImplementation("io.cucumber:cucumber-junit:7.12.1")
     testImplementation("io.cucumber:cucumber-core:7.12.1")
     testImplementation("io.cucumber:cucumber-java8:7.12.1")
@@ -34,30 +31,5 @@ dependencies {
 }
 
 tasks.test {
-//    systemProperty("cucumber.execution.parallel.enabled", "true")
-//    systemProperty("cucumber.execution.parallel.config.strategy", "fixed")
-//    systemProperty("cucumber.execution.parallel.config.fixed.parallelism", "2")
-
     useJUnitPlatform()
 }
-//
-//tasks {
-//
-//    val consoleLauncherTest by registering(JavaExec::class) {
-//        dependsOn(testClasses)
-//        val reportsDir = file("$buildDir/test-results")
-//        outputs.dir(reportsDir)
-//        classpath = sourceSets["test"].runtimeClasspath
-//        main = "org.junit.platform.console.ConsoleLauncher"
-//        args("--scan-classpath")
-//        args("--include-engine", "cucumber")
-//        args("--reports-dir", reportsDir)
-//    }
-//
-//    test {
-////        useJUnitPlatform()
-////        systemProperty("cucumber.junit-platform.naming-strategy", "long")
-//        dependsOn(consoleLauncherTest)
-//        exclude("**/*")
-//    }
-//}
